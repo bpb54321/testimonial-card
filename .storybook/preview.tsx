5cc2a9e614ba83fe3f302b0a87d6c690334caa06
Brian Blosser <brian.blosser@fxinnovation.com>
2024-08-19 15:27:26 -0400

    Add font-noto-sans to Tailwind config and use it in global Storybook decorator.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { Preview } from "@storybook/react";
 import "../src/output.css";
 
@@ -10,6 +11,13 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <div className="font-noto-sans">
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default preview;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.tsx"],
+  content: ["./src/**/*.tsx", "index.html"],
   theme: {
+    fontFamily: {
+      "noto-sans": ["Noto Sans", "ui-sans-serif", "system-ui", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
