54c15ac403547bf4f341583e5a106bf3ae510f37
Brian Blosser <brian.blosser@fxinnovation.com>
2024-08-20 16:42:46 -0400

    Add more styles to the card.

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -2,11 +2,17 @@ import profileImgUrl from "../assets/profile-thumbnail.png";
 
 export const TestimonialCard: React.FC = () => {
   return (
-    <div>
-      <div className="header">
-        <img src={profileImgUrl} alt="A profile photo of Sarah Dole." />
-        <p>Sarah Dole</p>
-        <p>@sarahdole</p>
+    <div className="bg-white p-6 rounded-lg w-[340px] flex flex-col gap-4 shadow">
+      <div className="flex gap-4">
+        <img
+          className="w-12 h-12"
+          src={profileImgUrl}
+          alt="A profile photo of Sarah Dole."
+        />
+        <div>
+          <p>Sarah Dole</p>
+          <p>@sarahdole</p>
+        </div>
       </div>
       <blockquote className="font-normal text-base text-neutral-600">
         I've been searching for high-quality abstract images for my design
