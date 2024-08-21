4b457c53d29cb3efcad26d026d18fa8b87b5ad0f
Brian Blosser <brian.blosser@fxinnovation.com>
2024-08-21 14:25:27 -0400

    Customize the viewport selector in Storybook.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,6 +2,33 @@ import React from "react";
 import type { Preview } from "@storybook/react";
 import "../src/output.css";
 
+const customViewports = {
+  desktop: {
+    name: "Desktop",
+    styles: {
+      width: "1440px",
+      height: "768px",
+    },
+    type: "desktop",
+  },
+  tablet: {
+    name: "Tablet",
+    styles: {
+      width: "768px",
+      height: "1024px",
+    },
+    type: "tablet",
+  },
+  mobile: {
+    name: "Mobile",
+    styles: {
+      width: "375px",
+      height: "812px",
+    },
+    type: "mobile",
+  },
+};
+
 const preview: Preview = {
   parameters: {
     controls: {
@@ -9,6 +36,10 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    viewport: {
+      viewports: customViewports,
+      defaultViewport: "desktop",
     },
   },
   decorators: [
