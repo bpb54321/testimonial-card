2cf49cc758d8e48f09f00a48ff6bbe4528bbf38e
Brian Blosser <brian.blosser@fxinnovation.com>
2024-08-27 16:27:13 -0400

    Add visual tests for different screen sizes.

--- visual-tests/testimonial-card.spec.ts
+++ visual-tests/testimonial-card.spec.ts
@@ -2,10 +2,36 @@ import { test, expect } from "@playwright/test";
 
 test.describe("testimonial card", () => {
   test("desktop", async ({ page }) => {
+    await page.setViewportSize({
+      width: 1440,
+      height: 768,
+    });
     await page.goto(
-      "/iframe.html?args=&id=testimonialcard--desktop&viewMode=story"
+      "/iframe.html?args=&id=testimonialcard--primary&viewMode=story"
     );
 
-    await expect(page.getByTestId("testimonial-card")).toHaveScreenshot();
+    await expect(page).toHaveScreenshot();
+  });
+  test("tablet", async ({ page }) => {
+    await page.setViewportSize({
+      width: 768,
+      height: 1024,
+    });
+    await page.goto(
+      "/iframe.html?args=&id=testimonialcard--primary&viewMode=story"
+    );
+
+    await expect(page).toHaveScreenshot();
+  });
+  test("mobile", async ({ page }) => {
+    await page.setViewportSize({
+      width: 375,
+      height: 812,
+    });
+    await page.goto(
+      "/iframe.html?args=&id=testimonialcard--primary&viewMode=story"
+    );
+
+    await expect(page).toHaveScreenshot();
   });
 });
