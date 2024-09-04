73e2146598f78d968f5d841b8f5ca1c13f5f3292
Brian Blosser <brian.blosser@fxinnovation.com>
2024-09-04 19:14:35 -0400

    Display the card on the home page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { TestimonialCard } from "./components/TestimonialCard";
 
+const quote =
+  "I've been searching for high-quality abstract images for my design " +
+  "projects, and I'm thrilled to have found this platform. The variety and " +
+  "depth of creativity are astounding!";
+const name = "Sarah Dole";
+const handle = "@sarahdole";
+
 function App() {
-  return <TestimonialCard />;
+  return (
+    <div className="flex flex-row w-full justify-center pt-8">
+      <TestimonialCard quote={quote} name={name} handle={handle} />
+    </div>
+  );
 }
 
 export default App;
