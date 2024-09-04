e1fb6f0e0aa022500636b97382d6eba58d98f895
Brian Blosser <brian.blosser@fxinnovation.com>
2024-09-04 16:30:18 -0400

    Remove additional unused modules.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,9 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.tsx"],
   addons: [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
-    "@storybook/addon-styling-webpack",
     "@storybook/addon-a11y",
   ],
   framework: {
