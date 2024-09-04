e963069cabc5fe20042713187ef6e64d7979762c
Brian Blosser <brian.blosser@fxinnovation.com>
2024-09-04 16:32:23 -0400

    Remove Chromatic visual testing.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,6 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
   ],
